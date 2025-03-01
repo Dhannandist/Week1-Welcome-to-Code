eff618b31eebc39ef52cd7ed2f481fa40b5dfc8f
DHannandist <dyanggaha@gmail.com>
2025-03-01 16:54:27 +0700

    Quiz Part2 Submit

---
+++ quiz/part2/soal1.js
@@ -0,0 +1,10 @@
+let word = 'Javascript ';
+let second = 'is ';
+let third = 'awesome ';
+let fourth = 'and ';
+let fifth = 'I ';
+let sixth = 'love ';
+let seventh = 'it!';
+
+// Print the Output
+console.log(word + second + third + fourth + fifth + sixth + seventh);
---
+++ quiz/part2/soal3.js
@@ -0,0 +1,15 @@
+// Breaking sentence using Substring
+
+let word3 = 'wow Javascript is so cool';
+let exampleFirstWord3 = word3.substring(0, 3);
+let secondWord3 = word3.substring(4, 14);
+let thirdWord3 = word3.substring(15, 17);
+let fourthWord3 = word3.substring(18, 20);
+let fifthWord3 = word3.substring(21, 25);
+
+// Print the Output
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
